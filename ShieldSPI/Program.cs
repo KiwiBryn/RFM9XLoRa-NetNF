8a9995ca0049652a10ed634d9a040f052bb191f5
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-05-03 10:18:21 +1200

    First version which builds and works on STM32F429 Discovery & ESP32 WROOM in the form of Sparkfun 1 channel gateway.

--- ShieldSPI/Program.cs
+++ ShieldSPI/Program.cs
@@ -14,6 +14,8 @@
 // limitations under the License.
 //
 //---------------------------------------------------------------------------------
+//#define ST_STM32F429I_DISCOVERY       //nanoff --target ST_STM32F429I_DISCOVERY --update
+#define ESP32_WROOM_32_LORA_1_CHANNEL   //nanoff --target ESP32_WROOM_32 --serialport COM4 --update
 namespace devMobile.IoT.Rfm9x.ShieldSPI
 {
    using System;
@@ -22,28 +24,50 @@ namespace devMobile.IoT.Rfm9x.ShieldSPI
    using Windows.Devices.Gpio;
    using Windows.Devices.Spi;
 
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+   using nanoFramework.Hardware.Esp32;
+#endif
+
    public class Program
    {
+#if ST_STM32F429I_DISCOVERY
+      private const string SpiBusId = "SPI5";
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+      private const string SpiBusId = "SPI1";
+#endif
+
       public static void Main()
       {
+#if ST_STM32F429I_DISCOVERY
+         int ledPinNumber  = PinNumber('G', 14);
+         int chipSelectPinNumber = PinNumber('C', 2);
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+         int ledPinNumber = Gpio.IO17;
+         int chipSelectPinNumber = Gpio.IO16;
+#endif
+
          try
          {
             GpioController gpioController = GpioController.GetDefault();
-
-            GpioPin led = gpioController.OpenPin(PinNumber('G', 14));
+            GpioPin led = gpioController.OpenPin(ledPinNumber);
             led.SetDriveMode(GpioPinDriveMode.Output);
 
-            GpioPin chipSelectGpioPin = gpioController.OpenPin(PinNumber('C', 2));
-            chipSelectGpioPin.SetDriveMode(GpioPinDriveMode.Output);
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO12, DeviceFunction.SPI1_MISO);
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO13, DeviceFunction.SPI1_MOSI);
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO14, DeviceFunction.SPI1_CLOCK);
+#endif
 
-            var settings = new SpiConnectionSettings(chipSelectGpioPin.PinNumber)
+            var settings = new SpiConnectionSettings(chipSelectPinNumber)
             {
                ClockFrequency = 500000,
                Mode = SpiMode.Mode0,// From SemTech docs pg 80 CPOL=0, CPHA=0
                SharingMode = SpiSharingMode.Shared,
             };
 
-            using (SpiDevice device = SpiDevice.FromId("SPI5", settings))
+            using (SpiDevice device = SpiDevice.FromId(SpiBusId, settings))
             {
                Thread.Sleep(500);
 
@@ -52,7 +76,8 @@ namespace devMobile.IoT.Rfm9x.ShieldSPI
                   byte[] writeBuffer = new byte[] { 0x42 };
                   byte[] readBuffer = new byte[1];
 
-                  device.TransferSequential(writeBuffer, readBuffer);
+                  device.TransferSequential(writeBuffer, readBuffer); // STM32_F429_DISCO worked, ESP32_LORA_1_CHANNEL worked
+                  //device.TransferFullDuplex(writeBuffer, readBuffer); // STM32_F429_DISCO didn't work, ESP32_LORA_1_CHANNEL didn't work
 
                   Console.WriteLine(String.Format("Register 0x{0:x2} - Value 0X{1:x2}", 0x42, readBuffer[0]));
 
@@ -67,6 +92,7 @@ namespace devMobile.IoT.Rfm9x.ShieldSPI
          }
       }
 
+#if ST_STM32F429I_DISCOVERY
       static int PinNumber(char port, byte pin)
       {
          if (port < 'A' || port > 'J')
@@ -74,5 +100,6 @@ namespace devMobile.IoT.Rfm9x.ShieldSPI
 
          return ((port - 'A') * 16) + pin;
       }
+#endif
    }
 }
