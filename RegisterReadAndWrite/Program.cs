874b901c5b3fb78ffaf9b74da774ad2549c243cb
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-05-03 22:08:12 +1200

    Updated to use TransferFullDuplex, ESP32 support and updated NuGets

--- RegisterReadAndWrite/Program.cs
+++ RegisterReadAndWrite/Program.cs
@@ -14,6 +14,8 @@
 // limitations under the License.
 //
 //---------------------------------------------------------------------------------
+//#define ST_STM32F429I_DISCOVERY       //nanoff --target ST_STM32F429I_DISCOVERY --update
+#define ESP32_WROOM_32_LORA_1_CHANNEL   //nanoff --target ESP32_WROOM_32 --serialport COM4 --update
 namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
 {
    using System;
@@ -21,6 +23,10 @@ namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
 
    using Windows.Devices.Gpio;
    using Windows.Devices.Spi;
+
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+   using nanoFramework.Hardware.Esp32;
+#endif
 
    public sealed class Rfm9XDevice
    {
@@ -49,6 +55,18 @@ namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
          Thread.Sleep(10);
       }
 
+      public Rfm9XDevice(string spiPort, int chipSelectPin)
+      {
+         var settings = new SpiConnectionSettings(chipSelectPin)
+         {
+            ClockFrequency = 1000000,
+            Mode = SpiMode.Mode0,// From SemTech docs pg 80 CPOL=0, CPHA=0
+            SharingMode = SpiSharingMode.Shared,
+         };
+
+         rfm9XLoraModem = SpiDevice.FromId(spiPort, settings);
+      }
+
       public Byte RegisterReadByte(byte registerAddress)
       {
          byte[] writeBuffer = new byte[] { registerAddress &= RegisterAddressReadMask, 0x0 };
@@ -71,42 +89,45 @@ namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
 
       public byte[] RegisterRead(byte address, int length)
       {
-         byte[] writeBuffer = new byte[length + 1];
-         byte[] readBuffer = new byte[length + 1];
-         byte[] repyBuffer = new byte[length];
+         byte[] writeBuffer = new byte[length+1];
+         byte[] readBuffer = new byte[writeBuffer.Length];
+         byte[] replyBuffer = new byte[length];
 
          writeBuffer[0] = address &= RegisterAddressReadMask;
 
          rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
 
-         Array.Copy(readBuffer, 1, repyBuffer, 0, length);
+         Array.Copy(readBuffer, 1, replyBuffer, 0, length);
 
-         return repyBuffer;
+         return replyBuffer;
       }
 
       public void RegisterWriteByte(byte address, byte value)
       {
          byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, value };
+         byte[] readBuffer = new byte[writeBuffer.Length];
 
-         rfm9XLoraModem.Write(writeBuffer);
+         rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
       }
 
       public void RegisterWriteWord(byte address, ushort value)
       {
          byte[] valueBytes = BitConverter.GetBytes(value);
          byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, valueBytes[0], valueBytes[1] };
+         byte[] readBuffer = new byte[writeBuffer.Length];
 
-         rfm9XLoraModem.Write(writeBuffer);
+         rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
       }
 
       public void RegisterWrite(byte address, byte[] bytes)
       {
          byte[] writeBuffer = new byte[1 + bytes.Length];
+         byte[] readBuffer = new byte[writeBuffer.Length];
 
          Array.Copy(bytes, 0, writeBuffer, 1, bytes.Length);
          writeBuffer[0] = address |= RegisterAddressWriteMask;
 
-         rfm9XLoraModem.Write(writeBuffer);
+         rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
       }
 
       public void RegisterDump()
@@ -123,46 +144,79 @@ namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
 
    class Program
    {
+#if ST_STM32F429I_DISCOVERY
+      private const string SpiBusId = "SPI5";
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+      private const string SpiBusId = "SPI1";
+#endif
+
       static void Main()
       {
-         Rfm9XDevice rfm9XDevice = new Rfm9XDevice("SPI5", PinNumber('C', 2), PinNumber('C', 3));
+#if ST_STM32F429I_DISCOVERY
+         int chipSelectPinNumber = PinNumber('C', 2);
+         int resetPinNumber = PinNumber('C', 2);
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+         int chipSelectPinNumber = Gpio.IO16;
+#endif
 
-         rfm9XDevice.RegisterDump();
-
-         while (true)
+         try
          {
-            Console.WriteLine("Read RegOpMode (read byte)");
-            Byte regOpMode1 = rfm9XDevice.RegisterReadByte(0x1);
-            Console.WriteLine($"RegOpMode 0x{regOpMode1:x2}");
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO12, DeviceFunction.SPI1_MISO);
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO13, DeviceFunction.SPI1_MOSI);
+            Configuration.SetPinFunction(nanoFramework.Hardware.Esp32.Gpio.IO14, DeviceFunction.SPI1_CLOCK);
 
-            Console.WriteLine("Set LoRa mode and sleep mode (write byte)");
-            rfm9XDevice.RegisterWriteByte(0x01, 0b10000000);
-
-            Console.WriteLine("Read RegOpMode (read byte)");
-            Byte regOpMode2 = rfm9XDevice.RegisterReadByte(0x1);
-            Console.WriteLine($"RegOpMode 0x{regOpMode2:x2}");
-
-            Console.WriteLine("Read the preamble (read word)");
-            ushort preamble = rfm9XDevice.RegisterReadWord(0x20);
-            Console.WriteLine($"Preamble 0x{preamble:x2}");
-
-            Console.WriteLine("Set the preamble to 0x80 (write word)");
-            rfm9XDevice.RegisterWriteWord(0x20, 0x80);
-
-            Console.WriteLine("Read the center frequency (read byte array)");
-            byte[] frequencyReadBytes = rfm9XDevice.RegisterRead(0x06, 5);
-            Console.WriteLine($"Frequency Msb 0x{frequencyReadBytes[0]:x2} Mid 0x{frequencyReadBytes[1]:x2} Lsb 0x{frequencyReadBytes[2]:x2}");
-
-            Console.WriteLine("Set the center frequency to 916MHz (write byte array)");
-            byte[] frequencyWriteBytes = { 0xE4, 0xC0, 0x00 };
-            rfm9XDevice.RegisterWrite(0x06, frequencyWriteBytes);
+            Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SpiBusId, chipSelectPinNumber);
+#endif
+#if ST_STM32F429I_DISCOVERY
+            Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SpiBusId, chipSelectPinNumber, resetPinNumber);
+#endif
+            Thread.Sleep(500);
 
             rfm9XDevice.RegisterDump();
 
-            Thread.Sleep(30000);
+            while (true)
+            {
+               Console.WriteLine("Read RegOpMode (read byte)");
+               Byte regOpMode1 = rfm9XDevice.RegisterReadByte(0x1);
+               Console.WriteLine($"RegOpMode 0x{regOpMode1:x2}");
+
+               Console.WriteLine("Set LoRa mode and sleep mode (write byte)");
+               rfm9XDevice.RegisterWriteByte(0x01, 0b10000000);
+
+               Console.WriteLine("Read RegOpMode (read byte)");
+               Byte regOpMode2 = rfm9XDevice.RegisterReadByte(0x1);
+               Console.WriteLine($"RegOpMode 0x{regOpMode2:x2}");
+
+               Console.WriteLine("Read the preamble (read word)");
+               ushort preamble = rfm9XDevice.RegisterReadWord(0x20);
+               Console.WriteLine($"Preamble 0x{preamble:x2}");
+
+               Console.WriteLine("Set the preamble to 0x80 (write word)");
+               rfm9XDevice.RegisterWriteWord(0x20, 0x80);
+
+               Console.WriteLine("Read the center frequency (read byte array)");
+               byte[] frequencyReadBytes = rfm9XDevice.RegisterRead(0x06, 3);
+               Console.WriteLine($"Frequency Msb 0x{frequencyReadBytes[0]:x2} Mid 0x{frequencyReadBytes[1]:x2} Lsb 0x{frequencyReadBytes[2]:x2}");
+
+               Console.WriteLine("Set the center frequency to 915MHz (write byte array)");
+               byte[] frequencyWriteBytes = { 0xE4, 0xC0, 0x00 };
+               rfm9XDevice.RegisterWrite(0x06, frequencyWriteBytes);
+
+               rfm9XDevice.RegisterDump();
+
+               Thread.Sleep(30000);
+            }
+         }
+         catch (Exception ex)
+         {
+            Console.WriteLine(ex.Message);
          }
       }
 
+#if ST_STM32F429I_DISCOVERY
       static int PinNumber(char port, byte pin)
       {
          if (port < 'A' || port > 'J')
@@ -170,5 +224,6 @@ namespace devMobile.IoT.Rfm9x.RegisterReadAndWrite
 
          return ((port - 'A') * 16) + pin;
       }
+#endif
    }
 }
