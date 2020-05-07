2b602a4b4e9f51ce6510d78fcccdb14685c1db1f
Bryn Howell Lewis <bryn.lewis@devmobile.co.nz>
2020-05-07 16:08:52 +1200

    First working version of standard demo application.

---
+++ Rfm9XLoRaDevice/RegisterManager.cs
@@ -0,0 +1,125 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) March/April 2020, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+namespace devMobile.IoT.Rfm9x
+{
+	using System;
+
+	using Windows.Devices.Gpio;
+	using Windows.Devices.Spi;
+
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+   using nanoFramework.Hardware.Esp32;
+#endif
+
+	public sealed class RegisterManager
+	{
+		private SpiDevice rfm9XLoraModem;
+		private const byte RegisterAddressReadMask = 0X7f;
+		private const byte RegisterAddressWriteMask = 0x80;
+
+		public RegisterManager(string spiPortName, int chipSelectPin, int clockFrequency = 500000)
+		{
+			GpioPin chipSelectGpio = GpioController.GetDefault().OpenPin(chipSelectPin);
+
+			var settings = new SpiConnectionSettings(chipSelectPin)
+			{
+				Mode = SpiMode.Mode0,
+				ClockFrequency = clockFrequency,
+				SharingMode = SpiSharingMode.Shared,
+			};
+
+			rfm9XLoraModem = SpiDevice.FromId(spiPortName, settings);
+		}
+
+		public Byte ReadByte(byte registerAddress)
+		{
+			byte[] writeBuffer = new byte[] { registerAddress &= RegisterAddressReadMask, 0x0 };
+			byte[] readBuffer = new byte[writeBuffer.Length];
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
+
+			return readBuffer[1];
+		}
+
+		public ushort ReadWord(byte address)
+		{
+			byte[] writeBuffer = new byte[] { address &= RegisterAddressReadMask, 0x0, 0x0 };
+			byte[] readBuffer = new byte[writeBuffer.Length];
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
+
+			return (ushort)(readBuffer[2] + (readBuffer[1] << 8));
+		}
+
+		public byte[] Read(byte address, int length)
+		{
+			byte[] writeBuffer = new byte[length + 1];
+			byte[] readBuffer = new byte[length + 1];
+			byte[] replyBuffer = new byte[length];
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			writeBuffer[0] = address &= RegisterAddressReadMask;
+
+			rfm9XLoraModem.TransferFullDuplex(writeBuffer, readBuffer);
+
+			Array.Copy(readBuffer, 1, replyBuffer, 0, length);
+
+			return replyBuffer;
+		}
+
+		public void WriteByte(byte address, byte value)
+		{
+			byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, value };
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			rfm9XLoraModem.Write(writeBuffer);
+		}
+
+		public void WriteWord(byte address, ushort value)
+		{
+			byte[] valueBytes = BitConverter.GetBytes(value);
+			byte[] writeBuffer = new byte[] { address |= RegisterAddressWriteMask, valueBytes[0], valueBytes[1] };
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			rfm9XLoraModem.Write(writeBuffer);
+		}
+
+		public void Write(byte address, byte[] bytes)
+		{
+			byte[] writeBuffer = new byte[1 + bytes.Length];
+			//Debug.Assert(rfm9XLoraModem != null);
+
+			Array.Copy(bytes, 0, writeBuffer, 1, bytes.Length);
+			writeBuffer[0] = address |= RegisterAddressWriteMask;
+
+			rfm9XLoraModem.Write(writeBuffer);
+		}
+
+		public void Dump(byte start, byte finish)
+		{
+			Console.WriteLine("Register dump");
+			for (byte registerIndex = 0; registerIndex <= 0x42; registerIndex++)
+			{
+				byte registerValue = this.ReadByte(registerIndex);
+
+				Console.WriteLine($"Register 0x{registerIndex:x2} - Value 0X{registerValue:x2}");
+			}
+		}
+	}
+}
---
+++ Rfm9xLoRaDeviceClient/Program.cs
@@ -0,0 +1,146 @@
+﻿//---------------------------------------------------------------------------------
+// Copyright (c) April/May 2020, devMobile Software
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//---------------------------------------------------------------------------------
+//#define ADDRESSED_MESSAGES_PAYLOAD
+//#define ST_STM32F429I_DISCOVERY       //nanoff --target ST_STM32F429I_DISCOVERY --update
+#define ESP32_WROOM_32_LORA_1_CHANNEL   //nanoff --target ESP32_WROOM_32 --serialport COM4 --update
+namespace devMobile.IoT.Rfm9x.LoRaDeviceClient
+{
+	using System;
+	using System.Text;
+	using System.Threading;
+
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+	using nanoFramework.Hardware.Esp32;
+#endif
+
+	using devMobile.IoT.Rfm9x;
+
+	class Program
+	{
+		const double Frequency = 915000000.0;
+#if ST_STM32F429I_DISCOVERY
+		private const string SpiBusId = "SPI5";
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+		private const string SpiBusId = "SPI1";
+#endif
+
+		static void Main()
+		{
+			byte MessageCount = System.Byte.MaxValue;
+#if ADDRESSED_MESSAGES_PAYLOAD
+			const string HostName = "ESP32LoRa";
+			const string DeviceName = "LoRaIoT1";
+#else
+			const string DeviceName = "ESP32LoRa";
+#endif
+#if ST_STM32F429I_DISCOVERY
+			int chipSelectPinNumber = PinNumber('C', 2);
+			int resetPinNumber = PinNumber('C', 3);
+			int interruptPinNumber = PinNumber('A', 4);
+#endif
+#if ESP32_WROOM_32_LORA_1_CHANNEL
+         int chipSelectPinNumber = Gpio.IO16;
+         int interruptPinNumber = Gpio.IO26;
+
+			Configuration.SetPinFunction(Gpio.IO12, DeviceFunction.SPI1_MISO);
+			Configuration.SetPinFunction(Gpio.IO13, DeviceFunction.SPI1_MOSI);
+			Configuration.SetPinFunction(Gpio.IO14, DeviceFunction.SPI1_CLOCK);
+
+			Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SpiBusId, chipSelectPinNumber, interruptPinNumber);
+#endif
+#if ST_STM32F429I_DISCOVERY
+			Rfm9XDevice rfm9XDevice = new Rfm9XDevice(SpiBusId, chipSelectPinNumber, resetPinNumber, interruptPinNumber);
+#endif
+			rfm9XDevice.Initialise(Frequency, paBoost: true, rxPayloadCrcOn: true);
+
+#if DEBUG
+			rfm9XDevice.RegisterDump();
+#endif
+
+			rfm9XDevice.OnReceive += Rfm9XDevice_OnReceive;
+#if ADDRESSED_MESSAGES_PAYLOAD
+			rfm9XDevice.Receive(UTF8Encoding.UTF8.GetBytes(DeviceName));
+#else
+			rfm9XDevice.Receive();
+#endif
+			rfm9XDevice.OnTransmit += Rfm9XDevice_OnTransmit;
+
+			Thread.Sleep(10000);
+
+			while (true)
+			{
+				string messageText = string.Format("Hello from {0} ! {1}", DeviceName, MessageCount);
+				MessageCount -= 1;
+
+				byte[] messageBytes = UTF8Encoding.UTF8.GetBytes(messageText);
+				Console.WriteLine(string.Format("{0}-TX {1} byte message {2}", DateTime.UtcNow.ToString("HH:mm:ss"), messageBytes.Length, messageText));
+#if ADDRESSED_MESSAGES_PAYLOAD
+				rfm9XDevice.Send(UTF8Encoding.UTF8.GetBytes(HostName), messageBytes);
+#else
+				rfm9XDevice.Send(messageBytes);
+#endif
+				Thread.Sleep(10000);
+			}
+		}
+
+		private static void Rfm9XDevice_OnReceive(object sender, Rfm9XDevice.OnDataReceivedEventArgs e)
+		{
+			try
+			{
+				// Remove unprintable characters from messages
+				for (int index = 0; index < e.Data.Length; index++)
+				{
+					if ((e.Data[index] < 0x20) || (e.Data[index] > 0x7E))
+					{
+						e.Data[index] = 0x20;
+					}
+				}
+
+				string messageText = UTF8Encoding.UTF8.GetString(e.Data, 0, e.Data.Length);
+
+#if ADDRESSED_MESSAGES_PAYLOAD
+				string addressText = UTF8Encoding.UTF8.GetString(e.Address, 0, e.Address.Length);
+
+				Console.WriteLine(string.Format(@"{0}-RX From {1} PacketSnr {2} Packet RSSI {3}dBm RSSI {4}dBm ={5} ""{6}""", DateTime.UtcNow.ToString("HH:mm:ss"), addressText, e.PacketSnr, e.PacketRssi, e.Rssi, e.Data.Length, messageText));
+#else
+				Console.WriteLine(string.Format(@"{0}-RX PacketSnr {1} Packet RSSI {2}dBm RSSI {3}dBm ={4} ""{5}""", DateTime.UtcNow.ToString("HH:mm:ss"), e.PacketSnr, e.PacketRssi, e.Rssi, e.Data.Length, messageText));
+#endif
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+		}
+
+		private static void Rfm9XDevice_OnTransmit(object sender, Rfm9XDevice.OnDataTransmitedEventArgs e)
+		{
+			Console.WriteLine( string.Format("{0}-TX Done", DateTime.UtcNow.ToString("HH:mm:ss")));
+		}
+
+#if ST_STM32F429I_DISCOVERY
+      static int PinNumber(char port, byte pin)
+      {
+         if (port < 'A' || port > 'J')
+            throw new ArgumentException();
+
+         return ((port - 'A') * 16) + pin;
+      }
+#endif
+	}
+}
+
